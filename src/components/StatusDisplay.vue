0d9a300fde289c84e0e0f9ca28aa0c433d7c1726
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-01-04 08:51:56 -0300

    chore: use constants in status display

--- src/components/StatusDisplay.vue
+++ src/components/StatusDisplay.vue
@@ -1,22 +1,36 @@
 <script setup lang="ts">
 import { useCharacterStore } from "@/stores/character";
 import { computed } from "vue";
+import { STATUS_MULTIPLIER, HEROIC_MULTIPLIER } from "@/assets/ts/constants";
 
 const character = useCharacterStore();
 
-const healthPoints = computed(() => character.attributes.constitution * 6);
-const energyPoints = computed(
-  () => (character.attributes.agility + character.attributes.strenght) * 6
+const healthPoints = computed(
+  () => character.attributes.constitution * STATUS_MULTIPLIER
 );
+
+const energyPoints = computed(
+  () =>
+    (character.attributes.agility + character.attributes.strenght) *
+    STATUS_MULTIPLIER
+);
+
 const magicPoints = computed(
-  () => (character.attributes.wisdom + character.attributes.intelligence) * 6
+  () =>
+    (character.attributes.wisdom + character.attributes.intelligence) *
+    STATUS_MULTIPLIER
+);
+
+const heroicPoints = computed(
+  () => character.attributes.charisma * HEROIC_MULTIPLIER
 );
 </script>
 
 <template>
   <div class="attribute-field">
-    <label for="healthPoints">Health Points: {{ healthPoints }}</label>
+    <label for="healthPoints"> Health Points: {{ healthPoints }}</label>
     <label for="energyPoints">Energy Points: {{ energyPoints }}</label>
     <label for="magicPoints">Magic Points: {{ magicPoints }}</label>
+    <label for="heroicPoints">Heroic Points: {{ heroicPoints }}</label>
   </div>
 </template>
