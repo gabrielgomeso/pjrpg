aa1aff49cb012167c6c40843acea49bbbbc8e9ab
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-01-25 08:55:26 -0300

    chore: create feats selection

---
+++ src/views/CharacterCreation/FeatsSelection.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { StepLayout } from "@/components/layout";
+</script>
+
+<template>
+  <StepLayout
+    :current-step="4"
+    :step-title="'Select two feats'"
+    :previous-step="'habilities_selection'"
+    :next-step="'items_selection'"
+  >
+  </StepLayout>
+</template>
+
+<style></style>
