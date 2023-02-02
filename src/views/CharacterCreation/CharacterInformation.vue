3014f8ae44ddb491dc4ee617ed425608101607a2
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-02-02 08:58:27 -0300

    chore: use radio button instead of select to race and group choices

--- src/views/CharacterCreation/CharacterInformation.vue
+++ src/views/CharacterCreation/CharacterInformation.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { ref, watch, onMounted } from "vue";
 import type { Ref } from "vue";
 import { StepLayout } from "@/components/layout";
 // import type { ICharacterData } from "@/data/models";
@@ -33,6 +33,10 @@ watch(selectedRace, (newSelectedRace) => {
 watch(selectedGroup, (newGroupSelected) => {
   character_data.value.group = newGroupSelected;
 });
+
+onMounted(() => {
+  if (character_data.value.race) setGroups(character_data.value.race);
+});
 </script>
 
 <template>
@@ -59,7 +63,24 @@ watch(selectedGroup, (newGroupSelected) => {
       />
     </label>
 
-    <label class="form-label" for="character-race">
+    <div class="card-list">
+      <label v-for="race in raceList" :key="race" :value="race">
+        <input
+          type="radio"
+          name="character-race"
+          v-model="selectedRace"
+          class="card-input-element"
+          :value="race"
+        />
+
+        <div class="panel panel-default card-input">
+          <div class="panel-heading">{{ race }}</div>
+          <div class="panel-body">This is the race {{ race }}</div>
+        </div>
+      </label>
+    </div>
+
+    <!-- <label class="form-label" for="character-race">
       Race:
       <select class="form-input" v-model="selectedRace" name="character-race">
         <option value="undefined" selected>Select characters race</option>
@@ -67,9 +88,26 @@ watch(selectedGroup, (newGroupSelected) => {
           {{ race }}
         </option>
       </select>
-    </label>
+    </label> -->
 
-    <label class="form-label" for="character-group">
+    <div class="card-list">
+      <label v-for="group in groupOptions" :key="group" :value="group">
+        <input
+          type="radio"
+          v-model="selectedGroup"
+          name="character-group"
+          class="card-input-element"
+          :checked="group == selectedGroup"
+        />
+
+        <div class="panel panel-default card-input">
+          <div class="panel-heading">{{ group }}</div>
+          <div class="panel-body">This is the group {{ group }}</div>
+        </div>
+      </label>
+    </div>
+
+    <!-- <label class="form-label" for="character-group">
       Group:
       <select class="form-input" v-model="selectedGroup" name="character-group">
         <option value="undefined" selected>Select characters group</option>
@@ -77,7 +115,7 @@ watch(selectedGroup, (newGroupSelected) => {
           {{ group }}
         </option>
       </select>
-    </label>
+    </label> -->
 
     <router-link
       class="form-button"
@@ -89,4 +127,27 @@ watch(selectedGroup, (newGroupSelected) => {
   </StepLayout>
 </template>
 
-<style></style>
+<style>
+.card-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.card-input-element {
+  display: none;
+}
+
+.card-input {
+  margin: 10px;
+  padding: 00px;
+}
+
+.card-input:hover {
+  cursor: pointer;
+}
+
+.card-input-element:checked + .card-input {
+  box-shadow: 0 0 1px 1px #2ecc71;
+}
+</style>
