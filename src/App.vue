19f1b04b126604afd92843477dea03cdc5921dfd
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-02-07 21:01:05 -0300

    style: add transition to global routing

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,27 @@ import { RouterLink, RouterView } from "vue-router";
     </nav>
   </header>
 
-  <RouterView class="main" />
+  <RouterView v-slot="{ Component }" mode="out-in" class="main">
+    <transition name="slide">
+      <component :is="Component" />
+    </transition>
+  </RouterView>
 </template>
 
 <style>
 .main {
   width: 100vw;
   height: var(--full-height);
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 1s, transform 1s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
 }
 </style>
