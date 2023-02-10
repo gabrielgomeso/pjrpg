590db13112949670ad6238ce9fb5bb1046805aea
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-02-10 09:08:12 -0300

    style: remove styles

--- src/components/layout/StepLayout.vue
+++ src/components/layout/StepLayout.vue
@@ -1,40 +1,21 @@
 <script setup lang="ts">
-import { FORM_STEPS } from "@/assets/ts/constants";
 defineProps<{
   stepTitle: string;
-  currentStep: number;
-  previousStep?: string;
-  nextStep?: string;
 }>();
 </script>
 
 <template>
-  <main class="character-creation-main">
-    <form class="character-form" action="" @submit.prevent>
-      <progress :value="currentStep" :max="FORM_STEPS"></progress>
-      <h2>{{ stepTitle }}</h2>
-      <slot></slot>
-
-      <router-link v-if="previousStep" class="form-button" :to="previousStep">
-        Previous
-      </router-link>
-      <router-link v-if="nextStep" class="form-button" :to="nextStep">
-        Next
-      </router-link>
-    </form>
+  <main class="step-layout--main">
+    <h2 class="step">{{ stepTitle }}</h2>
+    <slot></slot>
   </main>
 </template>
 
 <style>
-.character-creation-main {
+.step-layout--main {
   display: flex;
   align-items: center;
   justify-content: center;
-}
-
-.character-form {
-  display: grid;
-  gap: 1rem;
-  min-width: 50vw;
+  flex-direction: column;
 }
 </style>
