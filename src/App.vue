778f0758e6d2551124ad327d0ffc21bb43f083e5
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-02-12 12:21:40 -0300

    style: try to fix cls when page changes

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,45 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { RouterView } from "vue-router";
 import { TheHeader, TheFooter } from "@/components/layout";
+const hide = ref(false);
+
+function hideFooter() {
+  hide.value = true;
+}
+
+function showFooter() {
+  hide.value = false;
+}
 </script>
 
 <template>
-  <TheHeader />
-
-  <RouterView v-slot="{ Component }" class="main" mode="out-in">
-    <transition name="slide">
-      <component :is="Component" />
-    </transition>
-  </RouterView>
-
-  <TheFooter />
+  <main class="main-container">
+    <TheHeader />
+    <section class="main-container--router-view">
+      <RouterView v-slot="{ Component }" mode="out-in">
+        <transition
+          name="slide"
+          @before-enter="hideFooter"
+          @after-leave="showFooter"
+        >
+          <component :is="Component" />
+        </transition>
+      </RouterView>
+    </section>
+    <TheFooter :class="{ hidden: hide }" />
+  </main>
 </template>
 
 <style>
-.main {
+.main-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.main-container--router-view {
+  flex: 1;
+  position: relative;
   min-height: 100vh;
 }
 
@@ -24,7 +47,7 @@ import { TheHeader, TheFooter } from "@/components/layout";
 .slide-leave-active {
   display: block;
   position: absolute;
-  top: var(--navigation-size);
+  top: 0;
   left: 0;
   right: 0;
   bottom: 0;
@@ -34,7 +57,11 @@ import { TheHeader, TheFooter } from "@/components/layout";
 .slide-enter-from,
 .slide-leave-to {
   opacity: 0;
-  top: var(--navigation-size);
+  top: 0;
   transform: translateX(30%);
+}
+
+.hidden {
+  display: none;
 }
 </style>
