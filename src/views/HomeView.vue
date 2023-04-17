72942a26ca88fd3465eaff3d9f6036a4a69a7954
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-04-17 19:24:37 -0300

    style: add stupid grid in home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="home-main">Welcome, creature.</main>
+  <main class="home-main">
+    <div>Welcome, creature.</div>
+    <div>Zap</div>
+  </main>
 </template>
 <style>
 .home-main {
   display: grid;
+  grid-template-columns: 1fr 1fr;
   place-items: center;
   height: 100%;
 }
