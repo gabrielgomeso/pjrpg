ffa4cbe428910681ee2f9cf98bc8c13325eb08e5
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-07-02 23:41:45 -0300

    chore: create a welcome display

---
+++ src/components/HomeView/WelcomeDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="welcome-display__container">
+    <h3>Bem-vinde, aventureire.</h3>
+    <p>
+      Acesse seu perfil para modificar suas informações, criar seu personagem e
+      iniciar sua jornada.
+    </p>
+  </div>
+</template>
+<style>
+.welcome-display__container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 450px;
+  padding: 1rem;
+  border-radius: 15px;
+  background-color: #272727;
+}
+</style>
