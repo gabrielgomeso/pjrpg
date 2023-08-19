f2bd324cd144513ad8c1d62566edce0aefd8dd5f
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-08-19 19:18:34 -0300

    style: work in character preview style formatting

--- src/views/Characters/PreviewCharacter.vue
+++ src/views/Characters/PreviewCharacter.vue
@@ -1,13 +1,19 @@
 <script lang="ts" setup>
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 import { storeToRefs } from "pinia";
 import { useCharacterStore } from "@/stores/character";
 import { useFilters } from "@/composables/useFilters";
 import { supabase } from "@/lib/supabase";
 
-const { raceFilter } = useFilters();
+function capitalize(word: string) {
+  return word.charAt(0).toUpperCase() + word.substring(1);
+}
+
+const router = useRouter();
+const { raceFilter, statusFilter } = useFilters();
 const characterStore = useCharacterStore();
-const { character } = storeToRefs(characterStore);
+const { character, status } = storeToRefs(characterStore);
 
 const file = ref(null);
 
@@ -32,59 +38,183 @@ async function handleSubmit() {
     alert("Erro ao criar personagem!");
   }
 }
+
+function returnToNewCharacter() {
+  router.push({ name: "new_character" });
+}
+
+const isDemigod = (race: string) => race === "demigod";
 </script>
 
 <template>
   <section class="section-container">
     <header class="section-container__header">
-      <h1>Novo personagem</h1>
-      <p>Crie um novo personagem e faça parte do mundo de Percy Jackson!</p>
+      <h1>Resumo do novo personagem</h1>
+      <p>
+        Reveja todas as informações do seu novo personagem e prepare-se para a
+        aventura!
+      </p>
     </header>
 
-    <img
-      v-if="character.appeareance"
-      :src="character.appeareance"
-      alt="Imagem do seu personagem"
-      width="200"
-      height="400"
-    />
+    <div class="preview-character__character-info">
+      <figure class="preview-character__character-info-block">
+        <img
+          v-if="character.appeareance"
+          class="preview-character__image"
+          :src="character.appeareance"
+          alt="Imagem do seu personagem"
+          width="200"
+          height="400"
+        />
+      </figure>
 
-    <h2>{{ character.name }}</h2>
-    <h3>{{ raceFilter(character.race) }}</h3>
-    <h4>{{ character.group }}</h4>
+      <div class="preview-character__character-info-block">
+        <div
+          style="
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 5px;
+          "
+        >
+          <span>
+            <h2>
+              {{ character.name }},
+              <span v-if="isDemigod(character.race)">Filho(a) de</span>
+              {{ character.group }}
+            </h2>
+          </span>
+        </div>
+        <p>
+          Idade:
+          <span>{{ character.age }}</span>
+        </p>
 
-    <p>{{ character.age }}</p>
+        <p>
+          Origem:
+          <span>{{ character.origin }}</span>
+        </p>
 
-    <p>Hobbies:</p>
-    <p>{{ character.questions.hobby }}</p>
+        <div
+          style="
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: 1fr 1fr;
+            gap: 12px;
+            margin-top: 14px;
+          "
+        >
+          <div>
+            <h3>Hobbies</h3>
+            <p>{{ character.questions.hobby }}</p>
+          </div>
 
-    <p>Maior segredo:</p>
-    <p>{{ character.questions.secret }}</p>
+          <div>
+            <h3>Maior segredo</h3>
+            <p>{{ character.questions.secret }}</p>
+          </div>
 
-    <p>Maior medo:</p>
-    <p>{{ character.questions.fear }}</p>
+          <div>
+            <h3>Maior medo</h3>
+            <p>{{ character.questions.fear }}</p>
+          </div>
 
-    <p>Pessoas próximas:</p>
-    <p>{{ character.questions.family }}</p>
+          <div>
+            <h3>Pessoas próximas</h3>
+            <p>{{ character.questions.family }}</p>
+          </div>
 
-    <p>Maior sonho:</p>
-    <p>{{ character.questions.dream }}</p>
+          <div>
+            <h3>Maior sonho</h3>
+            <p>{{ character.questions.dream }}</p>
+          </div>
+        </div>
+      </div>
 
-    <span
-      v-for="(attribute, key) in character.attributes"
-      :key="attribute"
-      style="display: flex; flex-direction: column"
-    >
-      {{ key }}: {{ attribute }}
-    </span>
+      <div class="preview-character__character-info-block">
+        <h2>Atributos</h2>
+        <span
+          v-for="(attribute, key) in character.attributes"
+          :key="attribute"
+          style="display: flex; flex-direction: column"
+        >
+          {{ capitalize(key) }}: {{ attribute }}
+        </span>
+      </div>
 
-    <button
-      class="new-character__form-attribute-button"
-      @click="handleSubmit()"
-      type="submit"
-    >
-      Concluir
-    </button>
+      <div class="preview-character__character-info-block">
+        <h2>Status</h2>
+        <div class="preview-character__status">
+          <span v-for="(points, key) in status" :key="points">
+            <div class="preview-character__status-value">
+              {{ points }}
+            </div>
+            <span class="preview-character__status-name">
+              {{ statusFilter(key) }}
+            </span>
+          </span>
+        </div>
+      </div>
+    </div>
+
+    <details>
+      <summary>Vantagens e Desvantagens</summary>
+
+      <h2>Vantagens</h2>
+      <span v-for="(advantage, index) in character.advantages" :key="index">
+        <p>{{ advantage.name }} ({{ advantage.cost }})</p>
+        <p>{{ advantage.description }}</p>
+      </span>
+      <h2>Desvantagens</h2>
+      <span
+        v-for="(disadvantage, index) in character.disadvantages"
+        :key="index"
+      >
+        <p>{{ disadvantage.name }} ({{ disadvantage.cost }})</p>
+        <p>{{ disadvantage.description }}</p>
+      </span>
+    </details>
+
+    <hr />
+
+    <details>
+      <summary>Poderes divinos</summary>
+      <span
+        v-for="(initialPower, index) in character.initialPowers"
+        :key="index"
+      >
+        <p>{{ initialPower.name }}</p>
+        <p>{{ initialPower.description }}</p>
+      </span>
+    </details>
+
+    <hr />
+
+    <details>
+      <summary>Inventário</summary>
+      <span v-for="(item, index) in character.items" :key="index">
+        <p>{{ item.name }}</p>
+        <p>{{ item.description }}</p>
+      </span>
+    </details>
+
+    <div class="preview-character__buttons">
+      <button
+        class="new-character__form-attribute-button"
+        @click="returnToNewCharacter()"
+        type="submit"
+      >
+        Voltar
+      </button>
+
+      <button
+        class="new-character__form-attribute-button"
+        @click="handleSubmit()"
+        type="submit"
+      >
+        Criar novo personagem
+      </button>
+    </div>
   </section>
 </template>
 
@@ -113,5 +243,48 @@ async function handleSubmit() {
   margin: 1rem 0;
   border: none;
   border-bottom: 1px solid rgb(80, 80, 80);
+}
+
+.preview-character__character-info {
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  justify-content: space-evenly;
+  border: 1px solid black;
+}
+
+.preview-character__character-info-block {
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid black;
+}
+
+.preview-character__image {
+  object-fit: cover;
+}
+
+.preview-character__buttons {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+}
+
+.preview-character__status {
+  display: flex;
+  flex-direction: row;
+  margin-top: 8px;
+  text-align: center;
+  gap: 14px;
+}
+
+.preview-character__status-name {
+  margin-bottom: 14px;
+}
+
+.preview-character__status-value {
+  background-color: lightgray;
+  padding: 8px;
+  font-size: 1.5em;
 }
 </style>
