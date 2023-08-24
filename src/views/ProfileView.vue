bd1495e7b6639dd88e57624f119f48e33a48c976
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-08-23 22:43:42 -0300

    style: enhance profile style and add link to new character creation

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,58 +1,46 @@
 <script setup lang="ts">
-// import { ref, onMounted } from "vue";
-// import { useRouter } from "vue-router";
-
-// const router = useRouter();
-// const isLoading = ref(true);
-// const characters: any = ref(null);
-
-// async function findCharacter() {
-//   try {
-//     const character = await getCharacter(loggedUser.value.email);
-//     if (!character) {
-//       return;
-//     }
-//     characters.value = character;
-//   } catch (err) {
-//     isLoading.value = false;
-
-//     alert("Unknown error finding character" + err);
-//   } finally {
-//     isLoading.value = false;
-//   }
-// }
-
-// async function handleLogout() {
-//   try {
-//     // Call the login action from the auth store
-//     await logout();
-
-//     // If login is successful, redirect to /my-profile
-//     if (loggedUser.value == null) {
-//       router.push("/");
-//     }
-//   } catch (error: any) {
-//     // If there's an error, display the error message
-//     alert(error.message);
-//   }
-// }
-
-// onMounted(() => {
-//   findCharacter();
-// });
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
-  <div>
-    <h1>My profile</h1>
-    <h2>My Characters</h2>
+  <section class="section-container">
+    <header class="section-container__header">
+      <h1>Meu perfil</h1>
+      <p>Todas as informações da sua conta e de seus personagens!</p>
+    </header>
+    <h2>Meus personagens</h2>
+    <RouterLink
+      to="/characters/new"
+      class="profile-view__create-character-button"
+    >
+      Create new character
+    </RouterLink>
     <!-- <p v-if="isLoading">Loading characters...</p>
     <ul v-else>
       <li v-for="(character, index) in characters" :key="index">
         {{ character.character_info.character_data.name }}
       </li>
     </ul> -->
-  </div>
+  </section>
 </template>
 
-<style></style>
+<style>
+.profile-view__create-character-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: var(--vt-c-white);
+  width: 250px;
+  height: 350px;
+  background-color: rgb(182, 182, 182);
+  transition: transform 0.4s ease-in-out;
+  padding: 8px;
+}
+
+.profile-view__create-character-button:hover {
+  transform: scale(1.05);
+}
+</style>
