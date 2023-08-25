357f40611becc418232817a3416088e380c6f5ad
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-08-24 23:09:46 -0300

    chore: add user listing to profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,5 +1,38 @@
 <script setup lang="ts">
 import { RouterLink } from "vue-router";
+import { useAuthStore } from "@/stores/auth";
+import { onMounted, ref } from "vue";
+import { supabase } from "@/lib/supabase";
+
+const { user } = useAuthStore();
+
+const fetchedCharacters = ref<any>(null);
+
+async function getCharacters() {
+  try {
+    console.log(user);
+    const { data: character, error } = await supabase
+      .from("characters")
+      .select("character_info")
+      .eq("user_id", user.id);
+    if (error) {
+      alert(error.message);
+      console.error("There was an error inserting", error);
+      return null;
+    }
+
+    console.log(character);
+    fetchedCharacters.value = character;
+    return character;
+  } catch (err) {
+    alert("Error while fething data");
+    console.error("Unknown problem getting from the db", err);
+    return null;
+  }
+}
+onMounted(async () => {
+  await getCharacters();
+});
 </script>
 
 <template>
@@ -9,12 +42,26 @@ import { RouterLink } from "vue-router";
       <p>Todas as informações da sua conta e de seus personagens!</p>
     </header>
     <h2>Meus personagens</h2>
-    <RouterLink
-      to="/characters/new"
-      class="profile-view__create-character-button"
-    >
-      Create new character
-    </RouterLink>
+
+    <div class="profile-view__character-list">
+      <div
+        v-for="(character, index) in fetchedCharacters"
+        :key="index"
+        class="profile-view__create-character-button"
+      >
+        {{ character.character_info.name }}
+      </div>
+
+      <RouterLink
+        to="/characters/new"
+        class="profile-view__create-character-button"
+      >
+        Create new character
+      </RouterLink>
+    </div>
+
+    <button @click="getCharacters">clica</button>
+
     <!-- <p v-if="isLoading">Loading characters...</p>
     <ul v-else>
       <li v-for="(character, index) in characters" :key="index">
@@ -25,6 +72,12 @@ import { RouterLink } from "vue-router";
 </template>
 
 <style>
+.profile-view__character-list {
+  display: flex;
+  gap: 8px;
+  flex-wrap: wrap;
+}
+
 .profile-view__create-character-button {
   display: flex;
   align-items: center;
