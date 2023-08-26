696c385f66ae0a0fb3556c4085fb7ee601f4a7dc
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-08-26 16:13:09 -0300

    chore: import character sheet

--- src/views/Characters/index.ts
+++ src/views/Characters/index.ts
@@ -1,2 +1,3 @@
 export { default as NewCharacter } from "./NewCharacter.vue";
 export { default as PreviewCharacter } from "./PreviewCharacter.vue";
+export { default as CharacterSheet } from "./CharacterSheet.vue";
