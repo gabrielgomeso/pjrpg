b2f5a45c8c6f4c60332393c8eee34c42fc19c0d9
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-09-05 21:14:30 -0300

    chore: remove old pages there are now unused

--- src/views/CharacterCreation/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as CharacterRace } from "./CharacterRace.vue";
-export { default as CharacterGroup } from "./CharacterGroup.vue";
-export { default as AttributeSelection } from "./AttributeSelection.vue";
-export { default as HabilitiesSelection } from "./HabilitiesSelection.vue";
-export { default as FeatsSelection } from "./FeatsSelection.vue";
-export { default as ItemsSelection } from "./ItemsSelection.vue";
-export { default as CharacterOverview } from "./CharacterOverview.vue";
-export { default as CharacterInformation } from "./CharacterInformation.vue";
