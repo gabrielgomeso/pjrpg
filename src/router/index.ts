1d6caacd79cf63181cb59c635013121c1eebd549
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-09-05 21:24:27 -0300

    fix: remove unused routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,16 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 import { scrollToTop } from "@/assets/ts/utils";
 import HomeView from "../views/HomeView.vue";
 import {
-  AttributeSelection,
-  CharacterRace,
-  CharacterGroup,
-  HabilitiesSelection,
-  FeatsSelection,
-  ItemsSelection,
-  CharacterOverview,
-  CharacterInformation,
-} from "@/views/CharacterCreation";
-import {
   NewCharacter,
   PreviewCharacter,
   CharacterSheet,
@@ -63,54 +53,6 @@ const routes = [
         path: ":id",
         name: "character",
         component: CharacterSheet,
-      },
-    ],
-  },
-  {
-    path: "/character_creation",
-    name: "character_creation",
-    redirect: { name: "character_race" },
-    beforeEnter: authGuard,
-    children: [
-      {
-        path: "/character_race",
-        name: "character_race",
-        component: CharacterRace,
-      },
-      {
-        path: "/character_group",
-        name: "character_group",
-        component: CharacterGroup,
-      },
-      {
-        path: "/attribute_selection",
-        name: "attribute_selection",
-        component: AttributeSelection,
-      },
-      {
-        path: "/items_selection",
-        name: "items_selection",
-        component: ItemsSelection,
-      },
-      {
-        path: "/character_information",
-        name: "character_information",
-        component: CharacterInformation,
-      },
-      {
-        path: "/character_overview",
-        name: "character_overview",
-        component: CharacterOverview,
-      },
-      {
-        path: "/habilities_selection",
-        name: "habilities_selection",
-        component: HabilitiesSelection,
-      },
-      {
-        path: "/feats_selection",
-        name: "feats_selection",
-        component: FeatsSelection,
       },
     ],
   },
