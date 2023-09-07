441088d65a3f5738e4a20750410b2ca8fc4150a4
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-09-07 19:43:06 -0300

    chore: add about text

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="section-container">
+    <h1>SOBRE ESSE PROJETO</h1>
+    <p>
+      Esse é um projeto desenvolvido por um narrador de RPG. Eu queria criar um
+      local onde meus jogadores pudessem criar sua ficha e interagir com outros
+      aspectos do cenário que jogamos (o cenário de Percy Jackson) e assim esse
+      site foi criado.
+    </p>
+    <p>
+      O código está publicado para todos no Github. É tudo bem simples e eu
+      ficaria extremamente grato de auxiliar algum curioso caso haja alguma
+      dúvida ou interesse de replicar algo que fiz por aqui.
+    </p>
+    <p>
+      O sistema usado em nossa mesa de RPG foi criado por mim, juntando diversas
+      mecânicas de outros jogos que já joguei. Ele está disponível nessa página
+      pública do Notion. Ele é extremamente experimental e provavelmente
+      desbalanceado, então use por sua conta e risco.
+    </p>
+    <p>
+      Disclaimer: eu não possuo qualquer direito de imagem ao conteúdo da série
+      Percy Jackson. Todos os direitos são reservados a seu autor, Rick Riordan,
+      e provavelmente à Disney (que comprou o planeta Terra e tudo que há nele).
+    </p>
+  </section>
 </template>
 
 <style></style>
