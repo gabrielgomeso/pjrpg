711b0b6f83ca0f7627074b2201f549bcbc4555bb
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-09-13 23:04:54 -0300

    feat: create character avatar component

---
+++ src/components/Characters/CharacterSheet/CharacterAvatar.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const props = defineProps({
+  characterAvatarUrl: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <figure class="character-avatar__figure">
+    <img
+      class="character-avatar__image"
+      :src="props.characterAvatarUrl"
+      alt="Imagem do seu personagem"
+      width="200"
+      height="400"
+    />
+  </figure>
+</template>
+
+<style>
+.character-avatar__figure {
+  width: 200px;
+  height: 400px;
+}
+.character-avatar__image {
+  object-fit: cover;
+}
+</style>
---
+++ src/components/Characters/CharacterSheet/index.ts
@@ -0,0 +1,1 @@
+export { default as CharacterAvatar } from "./CharacterAvatar.vue";
