9883753ecac40cd6429d31286c58271e8e84d18c
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-09-16 14:11:29 -0300

    type: crreate iuser type

---
+++ src/data/models/IUser.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+  id: string;
+  aud: string;
+  role: string;
+  email: string;
+  created_at: string;
+  updated_at: string;
+}
--- src/data/models/index.ts
+++ src/data/models/index.ts
@@ -2,3 +2,4 @@ export * from "./IAttributes";
 export * from "./IStatus";
 export * from "./ICharacter";
 export * from "./ICharacterData";
+export * from "./IUser";
