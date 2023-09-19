b41584b863b8f3cd6717fce09b5c180527c1ae19
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-09-18 21:22:07 -0300

    type: update ICharacter type with new keys

--- src/data/models/ICharacter.ts
+++ src/data/models/ICharacter.ts
@@ -1,9 +1,23 @@
 import type { IAttributes } from "./IAttributes";
 import type { IStatus } from "./IStatus";
-import type { ICharacterData } from "./ICharacterData";
+import type { IQuestions } from "./IQuestions";
+import type { IAdvantage } from "./IAdvantage";
 
 export interface ICharacter {
+  name: string;
+  appeareance: string;
+  age: number;
+  race: string;
+  group: string;
+  origin: string;
+  questions: IQuestions;
   attributes: IAttributes;
+  items: [];
+  advantages: IAdvantage[];
+  disadvantages: IAdvantage[];
+  initialPowers: [];
   status: IStatus;
-  character_data: ICharacterData;
+  level: number;
+  experiencePoints: number;
+  userId: string;
 }
