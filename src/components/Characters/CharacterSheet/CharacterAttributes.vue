7c3c2a470c66af0f7f6583c2ad732899d60c46bd
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-09-18 21:05:06 -0300

    feat: create character attributes component

---
+++ src/components/Characters/CharacterSheet/CharacterAttributes.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import type { IAttributes } from "@/data/models";
+import { useFilters } from "@/composables/useFilters";
+const { capitalize } = useFilters();
+
+const props = defineProps<{
+  attributes: IAttributes;
+}>();
+</script>
+
+<template>
+  <div class="character-attributes">
+    <h2>Atributos</h2>
+    <span
+      v-for="(attribute, key) in props.attributes"
+      :key="key"
+      style="display: flex; flex-direction: column"
+    >
+      {{ capitalize(key) }}: {{ attribute }}
+    </span>
+  </div>
+</template>
+
+<style>
+.character-attributes {
+  width: 100%;
+  padding: 0.75rem;
+  border: 1px solid black;
+}
+</style>
--- src/components/Characters/CharacterSheet/index.ts
+++ src/components/Characters/CharacterSheet/index.ts
@@ -1,2 +1,3 @@
 export { default as CharacterAvatar } from "./CharacterAvatar.vue";
 export { default as CharacterGeneralInfo } from "./CharacterGeneralInfo.vue";
+export { default as CharacterAttributes } from "./CharacterAttributes.vue";
