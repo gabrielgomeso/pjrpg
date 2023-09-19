39c2b37a3250317a41e73db8b49d826d62d8f08f
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-09-18 21:12:31 -0300

    feat: create character status component

---
+++ src/components/Characters/CharacterSheet/CharacterStatus.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+import type { IStatus } from "@/data/models";
+import { useFilters } from "@/composables/useFilters";
+const { statusFilter } = useFilters();
+
+const props = defineProps<{
+  status: IStatus;
+}>();
+</script>
+
+<template>
+  <h2>Status</h2>
+  <div class="character-status">
+    <span v-for="(points, key) in props.status" :key="key">
+      <div class="character-status__value">
+        {{ points }}
+      </div>
+      <span class="character-status__name">
+        {{ statusFilter(key) }}
+      </span>
+    </span>
+  </div>
+</template>
+
+<style>
+.character-status {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  margin-top: 8px;
+  text-align: center;
+  gap: 14px;
+}
+
+.character-status__name {
+  margin-bottom: 14px;
+}
+
+.character-status__value {
+  background-color: lightgray;
+  padding: 8px;
+  font-size: 1.5em;
+}
+</style>
--- src/components/Characters/CharacterSheet/index.ts
+++ src/components/Characters/CharacterSheet/index.ts
@@ -1,3 +1,4 @@
 export { default as CharacterAvatar } from "./CharacterAvatar.vue";
 export { default as CharacterGeneralInfo } from "./CharacterGeneralInfo.vue";
 export { default as CharacterAttributes } from "./CharacterAttributes.vue";
+export { default as CharacterStatus } from "./CharacterStatus.vue";
