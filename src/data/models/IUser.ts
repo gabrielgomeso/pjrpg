e7956a814c1940f7b8ddd9a4117ea508e2e8421e
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-10-17 22:50:35 -0300

    type: change IUser typing

--- src/data/models/IUser.ts
+++ src/data/models/IUser.ts
@@ -1,8 +1,7 @@
 export interface IUser {
   id: string;
-  aud: string;
-  role: string;
-  email: string;
   created_at: string;
-  updated_at: string;
+  email: string;
+  username: string;
+  role: "user" | "admin";
 }
