4c4aef820d6ecc980a26281ce1c537bbef155aca
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-11-15 15:34:30 -0300

    feat: create forum view base

---
+++ src/views/Forum/ForumView.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="section-container">
+    <header class="section-container__header">
+      <h1>Fórum</h1>
+    </header>
+
+    <div>Auuu</div>
+  </section>
+</template>
+
+<style>
+.section-container {
+  max-width: 1024px;
+  border: 1px solid rgb(80, 80, 80);
+  margin: 1rem 1rem;
+  padding: 1rem;
+  container-type: inline-size;
+  background-color: whitesmoke;
+}
+
+.section-container__header {
+  margin-bottom: 1rem;
+  border-bottom: 1px solid rgb(80, 80, 80);
+}
+
+@media (min-width: 768px) {
+  .section-container {
+    margin: 1rem auto;
+  }
+}
+</style>
